3c35a83ec782b0e9c8ecf1c595a1f43666ecd3b9
luccskywalker <lucasemanuelalves@hotmail.com>
2023-05-21 05:13:21 -0300

    first commit for homepage

---
+++ script.js
@@ -0,0 +1,44 @@
+// Timeline data
+const timelineData = [
+  {
+    date: "2022-01-01",
+    description: "Event 1 description",
+  },
+  {
+    date: "2022-03-15",
+    description: "Event 2 description",
+  },
+  {
+    date: "2022-07-10",
+    description: "Event 3 description",
+  },
+  // Add more events as needed
+];
+
+// Function to create timeline items
+function createTimelineItem(date, description) {
+  const timelineItem = document.createElement("li");
+  timelineItem.classList.add("timeline-item");
+
+  const dateElement = document.createElement("div");
+  dateElement.classList.add("date");
+  dateElement.textContent = date;
+
+  const descriptionElement = document.createElement("div");
+  descriptionElement.classList.add("description");
+  descriptionElement.textContent = description;
+
+  timelineItem.appendChild(dateElement);
+  timelineItem.appendChild(descriptionElement);
+
+  return timelineItem;
+}
+
+// Populate the timeline
+const timeline = document.getElementById("timeline");
+
+timelineData.forEach((event) => {
+  const { date, description } = event;
+  const timelineItem = createTimelineItem(date, description);
+  timeline.appendChild(timelineItem);
+});
